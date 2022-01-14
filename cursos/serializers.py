151ff42d22334de6292b544482a33913a03e7f94
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-01-13 22:54:09 -0300

    Feito serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Course, Avaliation
+
+class AvaliationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Avaliation
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        fields = (
+            'id'
+            'name',
+            'email',
+            'comment',
+            'rate',
+            'course',
+            'active',
+            'created_at',
+            'updated_at',
+        )
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            'active',
+            'created_at',
+            'updated_at',
+            'avaliations',
+        )
+
