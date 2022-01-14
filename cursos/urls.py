02fea050abc50d8b1fa704d07d1fcd2bf8e100e0
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-01-14 19:15:05 -0300

    Usando generics

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import CourseAPIView, AvaliationAPIView
+from .views import CourseAPIView, CoursesAPIView, AvaliationAPIView, AvaliationsAPIView
 
 urlpatterns = [
-    path('courses/', CourseAPIView.as_view(), name='courses'),
-    path('avaliations/', AvaliationAPIView.as_view(), name='avaliations'),
+    path('courses/', CoursesAPIView.as_view(), name='courses'),
+    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+    path('avaliations/', AvaliationsAPIView.as_view(), name='avaliations'),
+    path('avaliations/<int:pk>/', AvaliationAPIView.as_view(), name='avaliations'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -4,10 +4,18 @@ from .models import Course, Avaliation
 from .serializers import AvaliationSerializer, CourseSerializer
 
 
-class CourseAPIView(generics.ListCreateAPIView):
+class CoursesAPIView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-class AvaliationAPIView(generics.ListCreateAPIView):
+class CourseAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class AvaliationsAPIView(generics.ListCreateAPIView):
+    queryset = Avaliation.objects.all()
+    serializer_class = AvaliationSerializer
+
+class AvaliationAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
