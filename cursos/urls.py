d0e6deaddf64463efeb067514df1120fd6a80f62
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-01-14 21:53:35 -0300

    Sobrescrevendo generics views

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,6 +5,9 @@ from .views import CourseAPIView, CoursesAPIView, AvaliationAPIView, Avaliations
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:course_pk>/avaliations/', AvaliationsAPIView.as_view(), name='course_avaliations'),
+    path('courses/<int:course_pk>/avaliations/<int:avaliation_pk>/', AvaliationAPIView.as_view(), name='course_avaliation'),
+
     path('avaliations/', AvaliationsAPIView.as_view(), name='avaliations'),
-    path('avaliations/<int:pk>/', AvaliationAPIView.as_view(), name='avaliations'),
+    path('avaliations/<int:avaliation_pk>/', AvaliationAPIView.as_view(), name='avaliations'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,7 @@
+from gc import get_objects
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Course, Avaliation
 from .serializers import AvaliationSerializer, CourseSerializer
@@ -16,6 +19,22 @@ class AvaliationsAPIView(generics.ListCreateAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('course_pk'):
+            return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+        return self.queryset.all()
+        
+
+
 class AvaliationAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
+
+    def get_object(self):
+        course_pk = self.kwargs.get('course_pk')
+        avaliation_pk = self.kwargs.get('avaliation_pk')
+        if course_pk:
+            return get_object_or_404(self.get_queryset(),
+                                    course_id=course_pk,
+                                    pk=avaliation_pk)
+        return get_object_or_404(self.get_queryset(), pk=avaliation_pk)
